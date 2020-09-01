3de7777064aa634a561d1c0cf07ee138000ba169
pomubry <btaduran30@protonmail.com>
2020-09-01 15:12:00 +0800

    Updates

--- precache-manifest.bacaaf4a0624141c2ce107c4aab06452.js
+++ precache-manifest.bacaaf4a0624141c2ce107c4aab06452.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "f46486f15564e571683788b3ca258e06",
+    "revision": "b62a4201dc2667881bf94df72b3469bf",
     "url": "/circle-animation/index.html"
   },
   {
-    "revision": "172b735ff8a7dd43e2fc",
-    "url": "/circle-animation/static/css/main.6d3e842e.chunk.css"
+    "revision": "d8fcd904d74296a9482f",
+    "url": "/circle-animation/static/css/main.4409c671.chunk.css"
   },
   {
     "revision": "0c837b4d5cee97a40044",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/circle-animation/static/js/2.6282f14d.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "172b735ff8a7dd43e2fc",
-    "url": "/circle-animation/static/js/main.54d06c24.chunk.js"
+    "revision": "d8fcd904d74296a9482f",
+    "url": "/circle-animation/static/js/main.2b143e3c.chunk.js"
   },
   {
     "revision": "9ce31c75d1bb1232fa1e",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/circle-animation/precache-manifest.c6be4712d3d60cda7336c352d25d5743.js"
+  "/circle-animation/precache-manifest.bacaaf4a0624141c2ce107c4aab06452.js"
 );
 
 self.addEventListener('message', (event) => {

72f49f17c78d26af20fcf28a23d0cc7144d0299c
pomubry <btaduran30@protonmail.com>
2021-01-09 16:43:13 +0800

    added dimension check

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,10 +1,28 @@
-import { useContext } from 'react';
+import { useContext, useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { AppContext } from './Reducers/appReducer';
 import { Helmet } from 'react-helmet-async';
 
 const Homepage = () => {
   const { state } = useContext(AppContext);
+  const [vw, setvw] = useState(0);
+  const [vh, setvh] = useState(0);
+
+  useEffect(() => {
+    function windowResize() {
+      setvw(window.innerWidth);
+      setvh(window.innerHeight);
+    }
+    window.addEventListener('resize', windowResize);
+
+    return () => window.removeEventListener('resize', windowResize);
+  });
+
+  const setDimensions = () => {
+    setvw(window.innerWidth);
+    setvh(window.innerHeight);
+  };
+
   return (
     <div className="homepage">
       <Helmet>
@@ -18,6 +36,9 @@ const Homepage = () => {
       ) : (
         <div style={{ margin: 'auto' }}>
           <p>Please login or register.</p>
+          <p>Height: {vh}</p>
+          <p>Width: {vw}</p>
+          <button onClick={setDimensions}>Set Dimensions</button>
         </div>
       )}
     </div>

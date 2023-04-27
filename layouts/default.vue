6c653734cd6c9ba4a33915f7f754fc968121b344
pomubry <btaduran30@protonmail.com>
2023-04-27 22:41:33 +0800

    add basic login and register route in default layout
       - add basic login/logout function in /login

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,34 @@
 <template>
-  <header>
+  <header
+    class="flex items-center justify-between p-5 text-lg font-extrabold underline-offset-8"
+  >
+    <nav class="flex flex-1 items-center justify-between">
+      <p class="text-2xl hover:underline">
+        <NuxtLink href="/">
+          C<span class="text-green-500 duration-300 dark:text-green-300"
+            >ani</span
+          >
+        </NuxtLink>
+      </p>
+      <ul class="mr-5 flex gap-3">
+        <li class="hover:underline">
+          <NuxtLink href="/login">Login</NuxtLink>
+        </li>
+        <li class="hover:underline">
+          <NuxtLink href="/register">Register</NuxtLink>
+        </li>
+      </ul>
+    </nav>
     <GenericThemeSwitch />
   </header>
 
   <slot />
-  <footer>Footer</footer>
+
+  <footer class="grid place-content-center p-5">
+    <p class="font-extrabold hover:underline">
+      <a href="https://pomubry.github.io" rel="noopener" target="_blank">
+        Pomubry <Icon name="mdi:github" />
+      </a>
+    </p>
+  </footer>
 </template>
---
+++ pages/login.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+const handleLogin = async () => {
+  console.log("Logging in");
+
+  const res = await fetch("http://localhost:5000/api/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      username: "ayumu",
+      password: "uehara",
+    }),
+    credentials: "include",
+  });
+  const data = await res.json();
+
+  console.log(data);
+};
+
+const handleLogout = async () => {
+  console.log("Logging out");
+
+  const res = await fetch("http://localhost:5000/api/logout", {
+    method: "GET",
+    credentials: "include",
+  });
+  const data = await res.json();
+
+  console.log(data);
+};
+</script>
+
+<template>
+  <h1>Login Component</h1>
+  <div class="flex gap-3">
+    <button @click="handleLogin">Login</button>
+    <button @click="handleLogout">Logout</button>
+  </div>
+</template>
